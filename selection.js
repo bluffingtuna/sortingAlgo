71207b1378c960949b578b80287a7163398aef72
bluffingtuna@gmail.com <bluffingtuna@gmail.com>
2017-12-14 16:58:55 -0800

    optimizing algo added to change the runtime of O(n) to O(log n)

--- selection.js
+++ selection.js
@@ -15,5 +15,7 @@ function selectionSort(arr) {
 	return arr;
 }
 
+arr2 = [4,2,6,4,12,5,3,12,6,8888,123,344]
 arr1 = [3,4,2,1,2,54,6,7,12,3,65]
 console.log(selectionSort(arr1))
+console.log(selectionSort(arr2))
